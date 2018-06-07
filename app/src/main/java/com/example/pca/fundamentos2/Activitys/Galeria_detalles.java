c8e3621cfde8d3c7f65afe328247bb381792976e
PcA <polohdzmg@gmail.com>
2018-06-08 01:35:54 +0200

    Cambios para galeria

--- app/src/main/java/com/example/pca/fundamentos2/Activitys/Galeria_detalles.java
+++ app/src/main/java/com/example/pca/fundamentos2/Activitys/Galeria_detalles.java
@@ -2,14 +2,119 @@ package com.example.pca.fundamentos2.Activitys;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import com.bumptech.glide.Glide;
+import com.example.pca.fundamentos2.Models.gallery_model;
 import com.example.pca.fundamentos2.R;
 
 public class Galeria_detalles extends AppCompatActivity {
+
+    private static final String EXTRA_NAME = "com.herprogramacion.toolbarapp.name";
+    private static final String EXTRA_DRAWABLE = "com.herprogramacion.toolbarapp.drawable";
+
+
+    public static void createInstance(Activity activity, gallery_model title) {//Girl es el pojo
+        Intent intent = getLaunchIntent(activity, title);
+        activity.startActivity(intent);
+    }
+
+    public static Intent getLaunchIntent(Context context, gallery_model girl) {
+        Intent intent = new Intent(context, Galeria_detalles.class);
+        intent.putExtra(EXTRA_NAME, girl.getName());
+        intent.putExtra(EXTRA_DRAWABLE, girl.getIdT());
+        return intent;
+    }
+
+    private void setToolbar() {
+        // Añadir la Toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    private void loadImageParallax(int id) {
+        ImageView image = (ImageView) findViewById(R.id.image_paralax);
+        // Usando Glide para la carga asíncrona
+        Glide.with(this)
+                .load(id)
+                .centerCrop()
+                .into(image);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.galeria_detalles_activity);
+
+        setToolbar();// Añadir action bar
+        if (getSupportActionBar() != null) // Habilitar up button
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        Intent i = getIntent();
+        String name = i.getStringExtra(EXTRA_NAME);
+        int idDrawable = i.getIntExtra(EXTRA_DRAWABLE, -1);
+
+        CollapsingToolbarLayout collapser =
+                (CollapsingToolbarLayout) findViewById(R.id.collapser);
+        collapser.setTitle(name); // Cambiar título
+
+        loadImageParallax(idDrawable);// Cargar Imagen
+
+        // Setear escucha al FAB
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        showSnackBar("Opción de Chatear");
+                    }
+                }
+        );
+
+
+    }
+
+    private void showSnackBar(String msg) {
+        Snackbar
+                .make(findViewById(R.id.coordinator), msg, Snackbar.LENGTH_LONG)
+                .show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.detail, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.action_settings:
+                showSnackBar("Se abren los ajustes");
+                return true;
+            case R.id.action_add:
+                showSnackBar("Añadir a contactos");
+                return true;
+            case R.id.action_favorite:
+                showSnackBar("Añadir a favoritos");
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
