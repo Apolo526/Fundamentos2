77d3807fe88437c05bb54d0dab1815325b596800
PcA <polohdzmg@gmail.com>
2018-06-07 21:14:25 +0200

    Nuevas Funciones 1

---
+++ app/src/main/java/com/example/pca/fundamentos2/Adapters/PagerAdapter.java
@@ -0,0 +1,24 @@
+package com.example.pca.fundamentos2.Adapters;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+public class PagerAdapter extends FragmentStatePagerAdapter{
+
+    int mNumOfTabs;
+
+    public PagerAdapter(FragmentManager fm , int NumOfTabs) {
+        super(fm);
+        this.mNumOfTabs = NumOfTabs;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
