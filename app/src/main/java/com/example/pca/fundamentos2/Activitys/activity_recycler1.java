33246da243e37f4224d169b4ab14293010033caf
PcA <polohdzmg@gmail.com>
2018-06-08 02:35:56 +0200

    Cambios para galeria

--- app/src/main/java/com/example/pca/fundamentos2/Activitys/activity_recycler1.java
+++ app/src/main/java/com/example/pca/fundamentos2/Activitys/activity_recycler1.java
@@ -1,15 +1,79 @@
 package com.example.pca.fundamentos2.Activitys;
 
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import com.example.pca.fundamentos2.Adapters.SimpleAdapter;
+import com.example.pca.fundamentos2.Adapters.gallery_adapter;
 import com.example.pca.fundamentos2.R;
-
 public class activity_recycler1 extends AppCompatActivity {
+
+    private RecyclerView recycler;
+    private LinearLayoutManager lManager;
+    private CollapsingToolbarLayout collapser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler1);
+
+
+        setToolbar();// Añadir la Toolbar
+        SimpleAdapter adaptador = new SimpleAdapter(this, gallery_adapter.randomList(30));
+
+        // Obtener el Recycler
+        recycler = (RecyclerView) findViewById(R.id.reciclador);
+        recycler.setHasFixedSize(true);
+
+        // Usar un administrador para LinearLayout
+        lManager = new LinearLayoutManager(this);
+        recycler.setLayoutManager(lManager);
+
+        // Crear un nuevo adaptador
+
+        recycler.setAdapter(adaptador);
+
+    }
+    private void setToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.action_search:
+                showSnackBar("Comenzar a buscar...");
+                return true;
+            case R.id.action_settings:
+                showSnackBar("Se abren los ajustes");
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    /**
+     * Proyecta una {@link Snackbar} con el string usado
+     *
+     * @param msg Mensaje
+     */
+    private void showSnackBar(String msg) {
+        Snackbar
+                .make(findViewById(R.id.coordinator), msg, Snackbar.LENGTH_LONG)
+                .show();
     }
 }
